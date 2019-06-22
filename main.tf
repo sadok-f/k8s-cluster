0e42c3ad08f051409ccdcf1104c63efb64b66718
Sadok Ferjani <sadoknet@gmail.com>
2019-06-22 22:53:40 +0200

    change vpc name

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "gke" {
   regional = false
   region = "${var.region}"
   zones = "${var.zones}"
-  network = "${google_compute_network.k8s-dev.id}"
+  network = "${google_compute_network.kiwi-vpc.id}"
   subnetwork = "${var.subnetwork_name}"
   ip_range_pods = "${var.ip_range_pods_name}"
   ip_range_services = "${var.ip_range_services_name}"
@@ -14,15 +14,15 @@ module "gke" {
 
 data "google_client_config" "default" {}
 
-resource "google_compute_network" "k8s-dev" {
-  name = "k8s-dev"
+resource "google_compute_network" "kiwi-vpc" {
+  name = "kiwi-vpc"
   auto_create_subnetworks = "false"
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
   name    = "${var.subnetwork_name}"
   ip_cidr_range = "${var.subnetwork_cidr}"
-  network = "${google_compute_network.k8s-dev.id}"
+  network = "${google_compute_network.kiwi-vpc.id}"
   region = "${var.region}"
 
   secondary_ip_range {
